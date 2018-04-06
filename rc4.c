b04f9b94e9cfaa884834c28487d1c3b61b18adfd
quandtifier <quandtifier@gmail.com>
2018-04-05 21:28:47 -0700

    the algorithm is written and appears to be working

--- rc4.c
+++ rc4.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
 #define BYTE_CONSTRAINT 256
+
 int main()
 {
    char *key = "HelloRC4c";
    printf(key);
+   printf("\n");
    // the bytes
    unsigned char bytes[BYTE_CONSTRAINT];
    int i;
@@ -18,13 +22,31 @@ int main()
      bytes[i] = bytes[j];
      bytes[j] = c;
    }
-
    for (i = 0; i < BYTE_CONSTRAINT; i++) {
      int j;
      for (j = 0; j < 8; j++) {
       printf("%d", !!((bytes[i] << j) & 0x80));
      }
-     printf("\n");
    }
+   printf("Line break\n");
+   printf("Line break\n");
+   j = i = 0;
+   int n;
+   char out[256];
+   for (n = 0; n < strlen(key); n++) {
+     i = (i + 1) % BYTE_CONSTRAINT;
+     j = (j + bytes[i]) % BYTE_CONSTRAINT;
+     char c = bytes[i];
+     bytes[i] = bytes[j];
+     bytes[j] = c;
+     out[n] = bytes[(bytes[i] + bytes[j]) % BYTE_CONSTRAINT];
+   }
+   for (i = 0; i < BYTE_CONSTRAINT; i++) {
+     int j;
+     for (j = 0; j < 8; j++) {
+      printf("%d", !!((out[i] << j) & 0x80));
+     }
+   }
+
    return 0;
 }
