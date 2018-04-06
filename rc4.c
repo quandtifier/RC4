6157b052eacfba1a542cbb08df9a12214aef2fdb
quandtifier <quandtifier@gmail.com>
2018-04-05 22:18:09 -0700

    have two different outputs for KSA algorithms

--- rc4.c
+++ rc4.c
@@ -3,18 +3,38 @@
 
 #define BYTE_CONSTRAINT 256
 
+void ksa(char k[], unsigned char s[]) {
+  int i;
+  for (i = 0; i < BYTE_CONSTRAINT; i++) {
+    s[i] = i;
+  }
+
+  // RC4 KSA (key scheduling algorithm)
+  int j = 0;
+  for (i = 0; i < BYTE_CONSTRAINT; i++) {
+    j = (j + s[i] + k[i % strlen(k)]) % BYTE_CONSTRAINT;
+    char c = s[i];
+    s[i] = s[i];
+    s[i] = c;
+  }
+}
+
 int main()
 {
    char *key = "HelloRC4c";
    printf(key);
    printf("\n");
-   // the bytes
+   // the bytes that start out as 0x00 and go through 0xFF
    unsigned char bytes[BYTE_CONSTRAINT];
+
+   //ksa(key, bytes);
+
    int i;
    for (i = 0; i < BYTE_CONSTRAINT; i++) {
      bytes[i] = i;
    }
 
+   // RC4 KSA (key scheduling algorithm)
    int j = 0;
    for (i = 0; i < BYTE_CONSTRAINT; i++) {
      j = (j + bytes[i] + key[i % strlen(key)]) % BYTE_CONSTRAINT;
@@ -22,14 +42,21 @@ int main()
      bytes[i] = bytes[j];
      bytes[j] = c;
    }
+
+   // print the bits for debugging
+   //int i;
    for (i = 0; i < BYTE_CONSTRAINT; i++) {
      int j;
      for (j = 0; j < 8; j++) {
       printf("%d", !!((bytes[i] << j) & 0x80));
      }
    }
+
+   // for debugging
    printf("Line break\n");
    printf("Line break\n");
+
+   // RSA PGRA (psuedo-random generation algorithm)
    j = i = 0;
    int n;
    char out[256];
@@ -41,6 +68,8 @@ int main()
      bytes[j] = c;
      out[n] = bytes[(bytes[i] + bytes[j]) % BYTE_CONSTRAINT];
    }
+
+   // print the bits for debugging
    for (i = 0; i < BYTE_CONSTRAINT; i++) {
      int j;
      for (j = 0; j < 8; j++) {
@@ -48,5 +77,14 @@ int main()
      }
    }
 
+   // for debugging check to ensure no duplicates valued bytes
+   for (i = 0; i < BYTE_CONSTRAINT; i++) {
+     int j;
+     for (j = 0; j < BYTE_CONSTRAINT; j++) {
+        if (bytes[i] == bytes[j] && i != j) {
+          printf("something went wrong there are dupicates!!!");
+        }
+     }
+   }
    return 0;
 }
